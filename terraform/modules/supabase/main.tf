71a3e93d74a19e44cfc48faac526b2b46b22f581
Thomas <sarpongthomas05@gmail.com>
2026-03-28 01:58:44 -0400

    ..

--- terraform/modules/supabase/main.tf
+++ terraform/modules/supabase/main.tf
@@ -25,20 +25,24 @@ data "supabase_apikeys" "this" {
 resource "supabase_settings" "auth" {
   project_ref = supabase_project.this.id
 
-  auth = jsonencode(merge(
-    {
-      site_url       = "https://${var.domains[0]}"
-      uri_allow_list = join(",", flatten([
-        for d in var.domains : [
-          "https://${d}/auth/callback",
-          "https://www.${d}/auth/callback"
-        ]
-      ]))
-    },
-    var.google_client_id != "" ? {
-      external_google_enabled   = true
-      external_google_client_id = var.google_client_id
-      external_google_secret    = var.google_client_secret
-    } : {}
-  ))
+  auth = var.google_client_id != "" ? jsonencode({
+    site_url                    = "https://${var.domains[0]}"
+    uri_allow_list              = join(",", flatten([
+      for d in var.domains : [
+        "https://${d}/auth/callback",
+        "https://www.${d}/auth/callback"
+      ]
+    ]))
+    external_google_enabled     = true
+    external_google_client_id   = var.google_client_id
+    external_google_secret      = var.google_client_secret
+  }) : jsonencode({
+    site_url       = "https://${var.domains[0]}"
+    uri_allow_list = join(",", flatten([
+      for d in var.domains : [
+        "https://${d}/auth/callback",
+        "https://www.${d}/auth/callback"
+      ]
+    ]))
+  })
 }
